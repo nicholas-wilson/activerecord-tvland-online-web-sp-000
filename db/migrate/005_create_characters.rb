c350927ea4e3b0b6f080b946a687e4d4b7e3f7b6
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-28 23:13:09 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,6 +1,7 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |c|
+      c.string :name
       c.string :first_name
       c.string :last_name
       c.string :catchphrase
