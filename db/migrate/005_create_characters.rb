1eaca9371f4f54043692e22a254952b003263a42
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-28 23:10:14 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -4,5 +4,7 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
       c.string :first_name
       c.string :last_name
       c.string :catchphrase
+      c.integer :show_id
+      c.integer :actor_id
   end
 end
