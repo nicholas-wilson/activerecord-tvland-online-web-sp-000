2a120ff67b0c8af37ab8f417b5338070ca9dd4a3
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-28 23:42:51 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    actors = []
+    self.characters.each do |character|
+      actors << character.actor.full_name
+    end
+    actors
+  end
 end
---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,12 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+  def change
+    create_table :characters do |c|
+      c.string :name
+      c.string :first_name
+      c.string :last_name
+      c.string :catchphrase
+      c.integer :show_id
+      c.integer :actor_id
+    end
+  end
+end
