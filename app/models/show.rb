8b487a6e7abf767a618bd706f480f727ffc5f338
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-28 23:40:10 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters
+
+  def actors_list
+    actors = []
+    self.characters.each do |character|
+      actors << character.actor.name
+    end
+  end
 end
